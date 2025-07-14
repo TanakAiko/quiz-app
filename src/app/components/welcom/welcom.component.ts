becc856a5307a10b3a2d38c7084b9920093827b5
TanakAiko <cherifmbaye02@gmail.com>
2025-07-14 16:51:52 +0000

    feat: implement welcome component with form for player name input

--- src/app/components/welcom/welcom.component.ts
+++ src/app/components/welcom/welcom.component.ts
@@ -1,12 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcom',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './welcom.component.html',
   styleUrl: './welcom.component.css'
 })
 export class WelcomComponent {
+  playerName: string = '';
 
+  constructor(private router: Router) { }
+
+  startGame() {
+    const name = this.playerName.trim();
+    if (name) {
+      this.router.navigate(['/quiz'], { queryParams: { name } });
+    } else {
+      alert('Please enter your name to start the game.');
+    }
+  }
 }
