8a23b259de160a332fa4e7a0f0d4886c1bb254d5
TanakAiko <cherifmbaye02@gmail.com>
2025-07-14 12:43:51 +0000

    add leaderboard, quiz, result, and welcome components with basic templates and tests

---
+++ src/app/components/welcom/welcom.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcom',
+  imports: [CommonModule],
+  templateUrl: './welcom.component.html',
+  styleUrl: './welcom.component.css'
+})
+export class WelcomComponent {
+
+}
